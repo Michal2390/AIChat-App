03c634285aa0f5ac6ddb4e86731353388d1b12b1
Michał Fereniec <michalkoks99@gmail.com>
2025-01-09 06:51:21 +0100

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Michal Fereniec on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
