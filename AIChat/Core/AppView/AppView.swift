3f022535668dc6be608afd7e5e4bddee196d6d41
Michał Fereniec <michalkoks99@gmail.com>
2025-01-13 14:41:48 +0100

    [Feature] Add AppView, AppViewBuilder

---
+++ AIChat/Core/AppView/AppView.swift
@@ -0,0 +1,39 @@
+//
+//  AppView.swift
+//  AIChat
+//
+//  Created by Michal Fereniec on 13/01/2025.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    
+    @AppStorage("showTabbarView") var showTabBar: Bool = false
+    
+    var body: some View {
+        AppViewBuilder(
+            showTabBar: showTabBar,
+            tabbarView: {
+                ZStack {
+                    Color.red.ignoresSafeArea()
+                    Text("Tabbar")
+                }
+            },
+            onboardingView: {
+                ZStack {
+                    Color.blue.ignoresSafeArea()
+                    Text("Onboarding")
+                }
+            }
+        )
+    }
+}
+
+#Preview("AppView - Tabbar") {
+    AppView(showTabBar: true)
+}
+
+#Preview("AppView - Onboarding") {
+    AppView(showTabBar: false)
+}
