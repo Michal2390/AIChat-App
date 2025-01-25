8e70967372fa94f092dfb10c60af58b2d21050d4
Michał Fereniec <michalkoks99@gmail.com>
2025-01-25 15:00:32 +0100

    [Feature] AvatarModel

--- AIChat/Core/Explore/ExploreView.swift
+++ AIChat/Core/Explore/ExploreView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct ExploreView: View {
+    
+    let avatar = AvatarModel.mock
+    
     var body: some View {
         NavigationStack {
-            Text("Explore")
-                .navigationTitle("Explore")
-        }    }
+            HeroCellView(
+                title: avatar.name,
+                subtitle: avatar.characterDescription,
+                imageName: avatar.profileImageName
+            )
+            .frame(height: 200)
+            .navigationTitle("Explore")
+            .padding(16)
+        }
+    }
 }
 
 #Preview {
