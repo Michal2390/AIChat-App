3f3da4c9fab9dd5accd6e4f9e700f5d9103d2b1d
Michał Fereniec <michalkoks99@gmail.com>
2025-02-18 06:09:52 +0100

    [Feature] SettingsView, CreateAccountView

--- AIChat/Core/Welcome/WelcomeView.swift
+++ AIChat/Core/Welcome/WelcomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct WelcomeView: View {
     
     @State var imageName: String = Constants.randomImage
+    @State private var showSignInView: Bool = false
     
     var body: some View {
         NavigationStack {
@@ -26,6 +27,13 @@ struct WelcomeView: View {
                 
                 policyLinks
             }
+        }
+        .sheet(isPresented: $showSignInView) {
+            CreateAccountView(
+                title: "Sign in",
+                subtitle: "Connect to an existing account"
+            )
+            .presentationDetents([.medium])
         }
     }
     
@@ -58,9 +66,13 @@ struct WelcomeView: View {
                 .padding(8)
                 .tappableBackground()
                 .onTapGesture {
-                    
+                    onSignInPressed()
                 }
         }
+    }
+    
+    private func onSignInPressed() {
+        showSignInView = true
     }
     
     private var policyLinks: some View {
---
+++ AIChat/Utilities/Utilities.swift
@@ -0,0 +1,9 @@
+//
+//  Utilities.swift
+//  AIChat
+//
+//  Created by Michal Fereniec on 18/02/2025.
+//
+import SwiftfulUtilities
+
+typealias Utilities = SwiftfulUtilities.Utilities
