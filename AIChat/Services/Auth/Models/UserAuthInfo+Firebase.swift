3c38d5318876a7a892c3308b79bd29179fc7e08d
Michał Fereniec <michalkoks99@gmail.com>
2025-03-24 23:59:52 +0100

    [Feature[ Sign in with Apple

---
+++ AIChat/Services/Auth/Models/UserAuthInfo+Firebase.swift
@@ -0,0 +1,38 @@
+//
+//  UserAuthInfo 2.swift
+//  AIChat
+//
+//  Created by Michal Fereniec on 24/03/2025.
+//
+
+
+
+struct UserAuthInfo: Sendable {
+    let uid: String
+    let email: String?
+    let isAnonymous: Bool
+    let creationDate: Date?
+    let lastSignInDate: Date?
+    
+    init(
+        uid: String,
+        email: String? = nil,
+        isAnonymous: Bool = false,
+        creationDate: Date? = nil,
+        lastSignInDate: Date? = nil
+    ) {
+        self.uid = uid
+        self.email = email
+        self.isAnonymous = isAnonymous
+        self.creationDate = creationDate
+        self.lastSignInDate = lastSignInDate
+    }
+
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email
+        self.isAnonymous = user.isAnonymous
+        self.creationDate = user.metadata.creationDate
+        self.lastSignInDate = user.metadata.lastSignInDate
+    }
+}
