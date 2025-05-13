9170db4c7f1d8447d17eb51f340b6e60b4f9f4b2
Michał Fereniec <michalkoks99@gmail.com>
2025-05-13 05:39:22 +0200

    [Feature] markChatMessageAsSeen, timestamp threshold message view

--- AIChat/Components/Views/ChatRowCellView/ChatRowCellViewBuilder.swift
+++ AIChat/Components/Views/ChatRowCellView/ChatRowCellViewBuilder.swift
@@ -55,7 +55,7 @@ struct ChatRowCellViewBuilder: View {
 
     private var hasNewChat: Bool {
         guard let lastChatMessage, let currentUserId else { return false }
-        return lastChatMessage.hasBeenSeenBy(userId: currentUserId)
+        return !lastChatMessage.hasBeenSeenBy(userId: currentUserId)
     }
 }
 
--- AIChat/Services/Chat/Services/ChatService.swift
+++ AIChat/Services/Chat/Services/ChatService.swift
@@ -11,8 +11,9 @@ protocol ChatService: Sendable {
     func getChat(userId: String, avatarId: String) async throws -> ChatModel?
     func getAllChats(userId: String) async throws -> [ChatModel]
     func addChatMessage(chatId: String, message: ChatMessageModel) async throws
+    func markChatMessageAsSeen(chatId: String, messageId: String, userId: String) async throws
     func getLastChatMessage(chatId: String) async throws -> ChatMessageModel?
-    @MainActor func streamChatMessages(chatId: String, onListenerConfigured: @escaping (AnyListener) -> Void ) -> AsyncThrowingStream<[ChatMessageModel], Error>
+    @MainActor func streamChatMessages(chatId: String, onListenerConfigured: @escaping (AnyListener) -> Void) -> AsyncThrowingStream<[ChatMessageModel], Error>
     func deleteChat(chatId: String) async throws
     func deleteAllChatsForUser(userId: String) async throws
     func reportChat(report: ChatReportModel) async throws
