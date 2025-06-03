7ab67d287b1e280d66652aef4a23d0595bd411c7
Michał Fereniec <michalkoks99@gmail.com>
2025-06-03 14:04:29 +0200

    [bug fix] building the project after adding new schemes vol.2 - resolved

---
+++ AIChat-Mock/AIChat_MockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChat_MockApp.swift
+//  AIChat-Mock
+//
+//  Created by Michal Fereniec on 03/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChat_MockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChat-MockTests/AIChat_MockTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChat_MockTests.swift
+//  AIChat-MockTests
+//
+//  Created by Michal Fereniec on 03/06/2025.
+//
+
+import Testing
+@testable import AIChat_Mock
+
+struct AIChat_MockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
