410301e70ee27457316717bb53d1fd7d30242063
Michał Fereniec <michalkoks99@gmail.com>
2025-06-23 14:31:35 +0200

    [Feature] Add Mixpanel

--- AIChat/Core/AIChatApp.swift
+++ AIChat/Core/AIChatApp.swift
@@ -95,7 +95,7 @@ struct Dependencies {
             avatarManager = AvatarManager(remote: FirebaseAvatarService(), local: SwiftDataLocalAvatarPersistence())
             chatManager = ChatManager(service: FirebaseChatService())
             logManager = LogManager(services: [
-                ConsoleService(), FirebaseAnalyticsService()
+                ConsoleService(), FirebaseAnalyticsService(), MixpanelService(token: Keys.mixpanel)
             ])
         case .prod:
             authManager = AuthManager(service: FirebaseAuthService())
@@ -104,7 +104,7 @@ struct Dependencies {
             avatarManager = AvatarManager(remote: FirebaseAvatarService(), local: SwiftDataLocalAvatarPersistence())
             chatManager = ChatManager(service: FirebaseChatService())
             logManager = LogManager(services: [
-                FirebaseAnalyticsService()
+                FirebaseAnalyticsService(), MixpanelService(token: Keys.mixpanel)
             ])
         }
     }
